494b9a830b609601d751d98ec62690f948b1a159
Hélio do Nascimento <115810349+Puritoka@users.noreply.github.com>
2024-04-25 11:06:34 -0300

    Manipulação de Dados em Python/Pandas - #06 Series

--- pds.py
+++ pds.py
@@ -1,15 +1,19 @@
 import pandas as pd
-from IPython.core.display_functions import display
+from self import self
 
 
-def star_wars():
-    imperio = pd.DataFrame({
-        'nome': ['Luke Skywalker', 'Yoda', 'Palpatine'],
-        'idade': [16, 1000, 70],
-        'peso': [70.5, 15.2, 60.1],
-        'eh jedi': [True, True, False]  # o nome das colunas podem ter espaços
-    })
-    display(imperio)
+class Gne:
+    def __init__(self):
+        self.tabelas = 'tabela'
+
+    def star_wars(self):
+        imperio = pd.DataFrame({
+            'nome': ['Luke Skywalker', 'Yoda', 'Palpatine'],
+            'idade': [16, 1000, 70],
+            'peso': [70.5, 15.2, 60.1],
+            'eh jedi': [True, True, False]  # o nome das colunas podem ter espaços
+        })
+        print(imperio)
 
 
-star_wars()
+Gne.star_wars(self)
